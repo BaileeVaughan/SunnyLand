c854cd6cbd4c75c8cb33946f10175805ec27736c
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-05-21 12:14:27 +1000

    1

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; //what is the camera following
+    public float smoothTime = .15f;//time to follow target
+    public float minXPos = 0, maxXPos = 0, minYPos = 0, maxYPos = 0; //min X/Y and max X/Y camera positions
+    public bool bounds; //bounds on or off
+
+    Vector3 velocity = Vector3.zero; //resets the velocity to zero
+    
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = target.position; //target position
+
+        if (bounds == true) //if bounds are on
+        {
+            targetPosition.y = Mathf.Clamp(target.position.y, minYPos, maxYPos);
+            targetPosition.x = Mathf.Clamp(target.position.x, minXPos, maxXPos);
+        }
+
+        targetPosition.z = transform.position.z; //align the camera and the target z position
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class Player : MonoBehaviour
+{
+    public float gravity = -10f;
+    public float movementSpeed = 10f;
+    public CharacterController2D controller;
+
+    private Vector3 motion;
+
+    void Reset()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal"); //get horizontal input
+
+        motion.x = inputH * movementSpeed;//move left and right
+
+        if (controller.isGrounded)
+        {
+            motion.y = 0f;
+        }
+
+        motion.y += gravity * Time.deltaTime; //apply gravity
+
+        controller.move(motion * Time.deltaTime); //apply movement with motion
+    }
+}
