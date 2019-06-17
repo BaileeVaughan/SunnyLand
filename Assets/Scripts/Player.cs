8096e2efcb83afd713c2769f89aca8fd68ab424d
BaileeVaughan <47824584+BaileeVaughan@users.noreply.github.com>
2019-06-17 13:32:44 +1000

    d

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,18 +6,25 @@ using Prime31;
 [RequireComponent(typeof(CharacterController2D))]
 public class Player : MonoBehaviour
 {
-    public float gravity = -10f, movementSpeed = 10f, jumpHeight = 5f;
+    public float gravity = -10f, movementSpeed = 10f, jumpHeight = 5f, centreRadius = .5f;
 
     private CharacterController2D controller;
     private Animator anim;
     private SpriteRenderer rend;
-    private Vector3 motion;
+    private Vector3 velocity;
+    private bool isClimbing = false;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, centreRadius);
+    }
 
     private void Start()
     {
         controller = GetComponent<CharacterController2D>();
         anim = GetComponent<Animator>();
-        rend = GetComponent<SpriteRenderer>();       
+        rend = GetComponent<SpriteRenderer>();
     }
 
     void Reset()
@@ -29,35 +36,40 @@ public class Player : MonoBehaviour
     {
         float inputV = Input.GetAxis("Vertical"); //get vertical input
         float inputH = Input.GetAxis("Horizontal"); //get horizontal input
-        if (!controller.isGrounded)
+        if (!controller.isGrounded && !isClimbing)
         {
-            motion.y += gravity * Time.deltaTime; //apply gravity
+            velocity.y += gravity * Time.deltaTime; //apply gravity
         }
         if (controller.isGrounded)
         {
             Jump();
         }
         Move(inputH);
-        Run();           
+        Climb(inputV, inputH);
+        Run();
         Crouch();
+
+        if (!isClimbing)
+        {
+            controller.move(velocity * Time.deltaTime); //apply movement with motion
+        }
     }
 
     void Move(float inputH)
     {
-        motion.x = inputH * movementSpeed;//move left and right
-        controller.move(motion * Time.deltaTime); //apply movement with motion
+        velocity.x = inputH * movementSpeed;//move left and right
         rend.flipX = inputH < 0;
     }
 
     void Jump()
-    {        
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            motion.y = jumpHeight;
+            velocity.y = jumpHeight;
             anim.SetBool("isJumping", true);
         }
         else
-        {            
+        {
             anim.SetBool("isJumping", false);
         }
     }
@@ -75,9 +87,47 @@ public class Player : MonoBehaviour
         }
     }
 
-    void Climb()
+    void Climb(float inputV, float inputH)
     {
-        anim.SetBool("isClimbing", true);
+        bool isOverLadder = false;
+
+        #region Detecting Ladders
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, centreRadius); // Get a list of all hit objects overlapping point        
+        foreach (var hit in hits) // Loop through all hit objects
+        {
+            if (hit.tag == "Ground")
+            {
+                isClimbing = false;
+                isOverLadder = false;
+            }
+
+            if (hit.tag == "Ladder") //Check if tagged "Ladder"
+            {
+                isOverLadder = true; //Player is overlapping a Ladder!
+                break; //exit just the foreach loop
+            }
+        }
+        if (isOverLadder && inputV != 0) // If the player is overlapping AND input vertical is made
+        {
+            isClimbing = true; //The player is in Climbing state!
+        }
+        #endregion
+
+        #region Translating the Player        
+        if (isClimbing) // If player is climbing
+        {
+            velocity.y = 0;
+            Vector3 inputDir = new Vector3(inputH, inputV, 0);
+            transform.Translate(inputDir* movementSpeed * Time.deltaTime); //Move player up and down on the ladder (additionally move left and right)
+        }
+
+        if (!isOverLadder)
+        {
+            isClimbing = false;
+        }
+        #endregion
+
+        anim.SetBool("isClimbing", isClimbing);
     }
 
     void Crouch()
